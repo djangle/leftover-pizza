f23994e80be61b1265938e95f68728eb7e12d27d
Adam Amodio <acamodio@gmail.com>
2022-01-18 10:25:13 -0800

    First commit.

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Leftover-Pizza
+Leftover Pizza is an adventure into game development with [Unity](https://www.unity.com).
---
+++ game/Assets/LintelController.cs
@@ -0,0 +1,23 @@
+// Inspiration: A lintel or lintol is a type of beam that spans openings such as portals, doors, windows and fireplaces. It can be a decorative architectural element, or a combined ornamented structural item.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LintelController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+
+
